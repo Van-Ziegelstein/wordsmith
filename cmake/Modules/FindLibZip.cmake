743a6b3d7d7da2e45a9bd59a18eff441ffd13836
Van Ziegelstein <vanziegelstein@gmail.com>
2019-06-06 00:07:44 +0200

    Add libzip find module for cmake.

---
+++ cmake/Modules/FindLibZip.cmake
@@ -0,0 +1,32 @@
+# Rudimentary module to find the libzip library.
+# 
+# Defines: 
+# 
+# LIBZIP_FOUND - Libzip library is installed
+# LIBZIP_INCLUDE_DIRS - Paths for the header include files
+# LIBZIP_LIBRARIES - Paths to link against the library
+#
+
+foreach(pkg ZLIB BZip2 OpenSSL GnuTLS PkgConfig)
+  find_package(${pkg} REQUIRED)
+endforeach()
+
+pkg_check_modules(PC_LIBZIP libzip)
+
+find_path(LIBZIP_INCLUDE_ZIP NAMES zip.h HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+
+find_path(LIBZIP_INCLUDE_ZIPCONF NAMES zipconf.h HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+
+find_library(LIBZIP_LIBRARY NAMES zip HINTS ${PC_LIBZIP_LIBRARY_DIRS})
+
+include(FindPackageHandleStandardArgs)
+find_package_handle_standard_args(LibZip DEFAULT_MSG
+                                  LIBZIP_INCLUDE_ZIP
+				  LIBZIP_INCLUDE_ZIPCONF
+				  LIBZIP_LIBRARY
+)
+
+mark_as_advanced(LIBZIP_INCLUDE_ZIP LIBZIP_INCLUDE_ZIPCONF LIBZIP_LIBRARY)
+set(LIBZIP_INCLUDE_DIRS ${LIBZIP_INCLUDE_ZIP} ${LIBZIP_INCLUDE_ZIPCONF})
+set(LIBZIP_LIBRARIES ${LIBZIP_LIBRARY})
+
