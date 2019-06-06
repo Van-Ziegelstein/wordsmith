66f20ec541690f9c96c6761696bcf2b13ce87f60
Van Ziegelstein <vanziegelstein@gmail.com>
2019-06-06 15:17:22 +0200

    Improve FindLibZip module.

--- cmake/Modules/FindLibZip.cmake
+++ cmake/Modules/FindLibZip.cmake
@@ -7,23 +7,39 @@
 # LIBZIP_LIBRARIES - Paths to link against the library
 #
 
-foreach(pkg ZLIB BZip2 OpenSSL GnuTLS PkgConfig)
+foreach(pkg ZLIB BZip2 OpenSSL GnuTLS)
   find_package(${pkg} REQUIRED)
 endforeach()
 
-pkg_check_modules(PC_LIBZIP libzip)
+find_package(PkgConfig QUIET)
 
-find_path(LIBZIP_INCLUDE_ZIP NAMES zip.h HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+if (PKG_CONFIG_FOUND)
+   pkg_check_modules(PC_LIBZIP QUIET libzip)
+endif()
 
-find_path(LIBZIP_INCLUDE_ZIPCONF NAMES zipconf.h HINTS ${PC_LIBZIP_INCLUDE_DIRS})
+find_path(LIBZIP_INCLUDE_ZIP 
+          NAMES zip.h 
+          HINTS ${PC_LIBZIP_INCLUDE_DIRS} ${CMAKE_INSTALL_PREFIX}
+          PATH_SUFFIXES include
+)
 
-find_library(LIBZIP_LIBRARY NAMES zip HINTS ${PC_LIBZIP_LIBRARY_DIRS})
+find_path(LIBZIP_INCLUDE_ZIPCONF 
+          NAMES zipconf.h 
+          HINTS ${PC_LIBZIP_INCLUDE_DIRS} ${CMAKE_INSTALL_PREFIX}
+          PATH_SUFFIXES include
+)
+
+find_library(LIBZIP_LIBRARY 
+             NAMES zip 
+             HINTS ${PC_LIBZIP_LIBRARY_DIRS} ${CMAKE_INSTALL_PREFIX}
+             PATH_SUFFIXES lib
+)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(LibZip DEFAULT_MSG
                                   LIBZIP_INCLUDE_ZIP
-				  LIBZIP_INCLUDE_ZIPCONF
-				  LIBZIP_LIBRARY
+                                  LIBZIP_INCLUDE_ZIPCONF
+                                  LIBZIP_LIBRARY
 )
 
 mark_as_advanced(LIBZIP_INCLUDE_ZIP LIBZIP_INCLUDE_ZIPCONF LIBZIP_LIBRARY)
